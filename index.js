0799b921ff0883d1e34cb9bd25927e84b139de3e
edumatheus65 <ematheusneves13@gmail.com>
2023-06-13 10:50:33 -0300

    Finalizado estrutura do projeto

--- index.js
+++ index.js
@@ -3,12 +3,57 @@ const exphbs = require('express-handlebars')
 const session = require('express-session')
 const FileStore = require('session-file-store')(session)
 const flash = require('express-flash')
-const sequelize = require('./db/conn')
+// const sequelize = require('./db/conn')
 const port = 3050
 
 const app = express()
 
 const conn = require('./db/conn')
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+// receber resposta do body
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+//session middleware
+app.use(
+    session({
+      name: 'session',
+      secret: 'nosso_secret',
+      resave: false,
+      saveUninitialized: false,
+      store: new FileStore({
+        logFn: function () {},
+        path: require('path').join(require('os').tmpdir(), 'sessions'),
+      }),
+      cookie: {
+        secure: false,
+        maxAge: 3600000,
+        expires: new Date(Date.now() + 3600000),
+        httpOnly: true,
+      },
+    }),
+  )
+// flash messages
+app.use(flash())
+
+// public path
+app.use(express.static('public'))
+
+// set session response
+app.use((req, res, next) => {
+    if(req.session.userid) {
+        res.locals.session = req.session
+    }
+    next()
+})
 
 conn
     .sync()
